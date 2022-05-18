a4901af4351309d84684d2ed54330f63ad782e68
mun-a <karnevall.13.13@gmail.com>
2022-05-18 20:24:28 +0500

    add displaying photos

--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -8,18 +8,19 @@ const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
-  const [loading, setLoading] = useState(falses);
+  const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
 
   const fetchImages = async () => {
     setLoading(true);
 
     let url;
-    url = `${main}${clientID}`;
+    url = `${mainUrl}${clientID}`;
 
     try {
       const response = await fetch(url);
       const data = await response.json();
+      setPhotos(data);
       setLoading(false);
     } catch (error) {
       setLoading(false);
@@ -31,7 +32,31 @@ function App() {
     fetchImages();
   }, []);
 
-  return <h2>stock photos starter</h2>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <main>
+      <section className="search">
+        <form className="search-form">
+          <input type="text" placeholder="search" className="form-input" />
+          <button type="submit" className="submit-btn" onClick={handleSubmit}>
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className="photos">
+        <div className="photos-center">
+          {photos.map((image) => (
+            <Photo key={image.id} {...image} />
+          ))}
+        </div>
+
+        {loading && <h2 className="loading">Loading...</h2>}
+      </section>
+    </main>
+  );
 }
 
 export default App;
