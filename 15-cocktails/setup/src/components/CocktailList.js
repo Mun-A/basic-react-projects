02c474309f4d714386f875d01ad38a8e34ec9a77
mun-a <karnevall.13.13@gmail.com>
2022-05-18 14:45:32 +0500

    add displaying cocktails

--- 15-cocktails/setup/src/components/CocktailList.js
+++ 15-cocktails/setup/src/components/CocktailList.js
@@ -4,13 +4,13 @@ import Loading from './Loading';
 import { useGlobalContext } from '../context';
 
 const CocktailList = () => {
-  const { loading, cocktail } = useGlobalContext();
+  const { loading, cocktails } = useGlobalContext();
 
   if (loading) {
     return <Loading />;
   }
 
-  if (cocktail.length < 1) {
+  if (cocktails.length < 1) {
     return (
       <h2 className="section-title">
         no cocktails matched your search criteria
@@ -19,9 +19,14 @@ const CocktailList = () => {
   }
 
   return (
-    <div>
-      <h2>cocktail list component</h2>
-    </div>
+    <section className="section">
+      <h2 className="section-title">cocktails</h2>
+      <div className="cocktails-center">
+        {cocktails.map((cocktail) => (
+          <Cocktail key={cocktail.id} {...cocktail} />
+        ))}
+      </div>
+    </section>
   );
 };
 
