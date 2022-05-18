3de7301e1ee7f6af682a37cafd35a928c4ea4ee6
mun-a <karnevall.13.13@gmail.com>
2022-05-18 20:03:18 +0500

    setting up and initial fetching

--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -1,12 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import { FaSearch } from 'react-icons/fa'
-import Photo from './Photo'
+import React, { useState, useEffect } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import Photo from './Photo';
+
 // const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
-const mainUrl = `https://api.unsplash.com/photos/`
-const searchUrl = `https://api.unsplash.com/search/photos/`
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
-  return <h2>stock photos starter</h2>
+  const [loading, setLoading] = useState(falses);
+  const [photos, setPhotos] = useState([]);
+
+  const fetchImages = async () => {
+    setLoading(true);
+
+    let url;
+    url = `${main}?client_id=wObn-8yHj7r0ag5czBNVHE_QtbaTk8dcv6gqXnH90v4`;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  return <h2>stock photos starter</h2>;
 }
 
-export default App
+export default App;
