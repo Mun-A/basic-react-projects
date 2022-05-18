e502e99263fb3442b90ad6b6e70f4394f36d05d3
mun-a <karnevall.13.13@gmail.com>
2022-05-18 14:16:57 +0500

    add navbar, about page and error page

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        officiis voluptatem accusamus rerum voluptates repellat alias, possimus
+        dolore placeat, quibusdam corrupti, corporis vitae molestias provident
+        veritatis animi explicabo modi harum.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
