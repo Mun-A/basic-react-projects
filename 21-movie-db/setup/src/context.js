d1323e79fa69538f5e84bd873c072a3134be9e10
mun-a <karnevall.13.13@gmail.com>
2022-05-19 12:54:33 +0500

    add useFetch() custom hook and refactor

--- 21-movie-db/setup/src/context.js
+++ 21-movie-db/setup/src/context.js
@@ -1,4 +1,6 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext } from 'react';
+
+import useFetch from './useFetch';
 
 // make sure to use https
 export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
@@ -6,34 +8,8 @@ export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState({ show: false, msg: '' });
-  const [movies, setMovies] = useState([]);
   const [query, setQuery] = useState('batman');
-
-  const fetchMovies = async (url) => {
-    setIsLoading(true);
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-
-      if (data.Response === 'True') {
-        setMovies(data.Search);
-        setError({ show: false, msg: '' });
-      } else {
-        setError({ show: true, msg: data.Error });
-      }
-
-      setIsLoading(false);
-    } catch (error) {
-      setIsLoading(false);
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchMovies(`${API_ENDPOINT}&s=${query}`);
-  }, [query]);
+  const { isLoading, error, data: movies } = useFetch(`&s=${query}`);
 
   return (
     <AppContext.Provider
